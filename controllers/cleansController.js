7f913ed939e8dc3cc37d7b2737e7932d0b285f89
Max Chamberlain <maxchamberlain@Maekaylas-MBP.balanceathletica.local>
2022-08-05 13:32:36 -0600

    cleans

---
+++ controllers/cleansController.js
@@ -0,0 +1,39 @@
+const Clean = require('../models/cleanModel')
+const asyncHandler = require('express-async-handler')
+
+const insertClean = async (req, res, next) => {
+    try{
+        const { barcode, user } = req.body
+
+        console.log(req.body)
+
+        const items = await Return.create({
+            barcode,
+            user,
+        })
+    
+        if(items){
+            res.status(201).send(items)
+        }else{
+            res.status(400).json({text: 'Invalid Order Number'})
+        }
+    }catch(e){
+        res.status(400).json({text: 'An Error Has Occurred. (Error Code: Jaguar)'})
+    }
+}
+
+const getAll = async (req, res, next) => {
+    try{
+        const items = await Clean.find({ })
+
+        if(items){
+            res.status(201).send(items)
+        }else{
+            res.status(400).json({text: 'Invalid Order Number'})
+        }
+    }catch(e){
+        res.status(400).json({text: 'An Error Has Occurred. (Error Code: Lemur)'})
+    }
+}
+
+module.exports = { insertReturn, processReturn, getAll }
---
+++ models/cleanModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const clean = mongoose.Schema(
+    {
+        barcode: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        user: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true,
+            default: 'Cleaned'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Clean = mongoose.model('clean', clean, 'cleaned_garments')
+
+module.exports = Clean
