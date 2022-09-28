4187ab63c0620cb82f6e6d9ce714a2afb7fee443
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-09-28 16:26:38 -0600

    sh

--- controllers/shipheroItemController.js
+++ controllers/shipheroItemController.js
@@ -4,10 +4,10 @@ const getItem = async (req, res) => {
     try{
         const { sku } = req.body
         const data = await get(sku)
-        res.status(200).json(data)
+        res.status(200).send(data)
     }catch(e){
         console.error(e)
-        res.status(500).json(e)
+        res.status(500).send(e)
     }
 }
 
